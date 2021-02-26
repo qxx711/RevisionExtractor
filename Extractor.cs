6ab4c700d3d9afa96a0390c7c95c40f5f971febb
qxx711 <31662188+qxx711@users.noreply.github.com>
2021-02-26 18:38:18 +0800

    Add files via upload

---
+++ Extractor.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace ReversionExtract
+{
+    public abstract class Extractor
+    {
+        public string path;
+        public string outputDir;
+        public List<List<object>> InputMatrix;
+        public int RowNum;
+        public static FastZip fz = new FastZip();
+
+        public int total = 30;
+        public int current { set; get; }
+        public uint starttime { set; get; }
+        public uint endtime { set; get; }
+        public abstract void doExtract();
+    }
+    class GitExtractor : Extractor
+    {
+        [DllImport("winmm")]
+        static extern uint timeGetTime();
+        public GitExtractor(string path, string outputDir, List<List<object>> InputMatrix, int RowNum)
+        {
+            this.path = path;
+            this.outputDir = outputDir;
+            this.InputMatrix = InputMatrix;
+            this.RowNum = RowNum;
+        }
+        public override void doExtract()
+        {
+            current = 0;
+            String projectName = System.IO.Path.GetFileNameWithoutExtension(path);
+            using (var repo = new Repository(path))
+            {
+                starttime = timeGetTime();
+                for (int i = 1; i < RowNum; i++)
+                {
+                    String hashCode = Convert.ToString(InputMatrix[i][0]);
+                    //git archive
+                    Commit cm = repo.Lookup<Commit>(hashCode);
+                    String tarpath = outputDir + "\\" + projectName;
+                    String untarpath = outputDir + "\\" + projectName + "\\" + hashCode;
+                    string tarfile = outputDir + "\\" + projectName + "\\" + hashCode+ ".zip";
+                    String tarname = hashCode + ".zip";
+                    if (!Directory.Exists(untarpath))
+                    {
+                        //创建
+                        Directory.CreateDirectory(untarpath);
+                    }
+                    repo.ObjectDatabase.Archive(cm, tarfile);
+                    UnRAR(untarpath, tarpath, tarname);
+                    current++;
+                }
+                endtime = timeGetTime();
+            }
+        }
+        /// <summary>
+        /// 利用 WinRAR 进行解压缩
+        /// </summary>
+        /// <param name="path">文件解压路径（绝对）</param>
+        /// <param name="rarPath">将要解压缩的 .rar 文件的存放目录（绝对路径）</param>
+        /// <param name="rarName">将要解压缩的 .rar 文件名（包括后缀）</param>
+        public void UnRAR(string path, string rarPath, string rarName)
+        {
+            ProcessStartInfo startinfo = new ProcessStartInfo(); 
+            Process process = new Process();
+            String rarexe = @"F:\WinRAR\WinRAR.exe"; //WinRAR安装位置
+            //解压缩命令，相当于在要压缩文件(rarName)上点右键->WinRAR->解压到当前文件夹
+            String cmd = string.Format("x {0} {1} -y",rarName,path);
+            startinfo.FileName = rarexe;
+            startinfo.Arguments = cmd;
+            startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startinfo.WorkingDirectory = rarPath;
+            process.StartInfo = startinfo;
+            process.Start();
+            process.WaitForExit();
+            process.Dispose();
+            process.Close();
+            bool isdeletezip = true;
+            if (isdeletezip) 
+            {
+                DeleteFile(rarPath + "\\"+rarName);
+            }
+        }
+        ///
+        /// 根据文件路径删除
+        ///
+        public static bool DeleteFile(string fPath)
+        {
+            if (File.Exists(fPath))
+            {
+                FileInfo fi = new FileInfo(fPath);
+                fi.Attributes = FileAttributes.Normal;
+                fi.Delete();               
+            }
+            return true;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+using LibGit2Sharp;
+using System.Threading;
+
+namespace ReversionExtract
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folder = new FolderBrowserDialog();
+        string OutputFolderPath = "";
+        Microsoft.Office.Interop.Excel.Application app = null;
+        int col;
+        int row;
+        private Thread t;
+        private int current = 0;
+        private int total = 1;
+        Extractor extractor;
+        private uint starttime;
+        private uint endtime;
+        private Boolean failToLink = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folder.ShowDialog();
+            textBox1.Text = folder.SelectedPath;
+        }
+     
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = dialog.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folder.ShowDialog();
+            textBox3.Text = folder.SelectedPath;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            progressPresent.ForeColor = Color.Green;
+            progressPresent.Text = "Running";
+
+            String gitPath = textBox1.Text;
+            String excelPath = textBox2.Text;
+            OutputFolderPath = textBox3.Text;
+
+            List<List<object>> InputMatrix;
+            int RowNum;
+            ReadExcel(excelPath, out InputMatrix, out RowNum);
+
+            extractor = new GitExtractor(textBox1.Text, textBox3.Text, InputMatrix, RowNum);
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+            progressPresent.Visible = true;
+            timer1.Start();
+            button4.Text = "Extracting...";
+            button4.Enabled = false;
+            button4.Update();
+            t = new Thread(new ThreadStart(extractor.doExtract));
+            t.IsBackground = true;
+            t.Start();
+            progressPresent.Text = "success!";            
+
+        }
+        public void ReadExcel(string FilePath, out List<List<object>> InputMatrix, out int RowNum)
+        {
+            Microsoft.Office.Interop.Excel.Workbook workBook = null;
+            app = new Microsoft.Office.Interop.Excel.Application();
+            workBook = app.Workbooks.Open(FilePath);
+            Worksheet worksheet = (Worksheet)workBook.Worksheets[5];
+            col = worksheet.UsedRange.CurrentRegion.Columns.Count;
+            row = worksheet.UsedRange.CurrentRegion.Rows.Count;
+            InputMatrix = NewListMatrixOfObject(row, col);
+            object[,] current;
+            current = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[row, col]].Value2;
+
+            int k = 1;
+            for (int i = 0; i <= row - 1; i++)
+            {
+                if (current[i + 1, 7] != null)
+                {
+                    for (int j = 1; j <= col; j++)
+                    {
+                        InputMatrix[k - 1][j - 1] = current[i + 1, j];
+                    }
+                    k++;
+                }
+            }
+            RowNum = k - 1;
+            app.Quit();
+            app = null;
+            workBook = null;
+        }
+        public static List<List<object>> NewListMatrixOfObject(int iRowCount, int iColCount)
+        {
+            List<List<object>> matrix = new List<List<object>>(iRowCount);
+            for (int i = 0; i < iRowCount; i++)
+            {
+                matrix.Add(new List<object>(iColCount));
+                for (int j = 0; j < iColCount; j++)
+                {
+                    matrix[i].Add("");
+                }
+            }
+            return matrix;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            current = extractor.current;
+            total = extractor.total;
+            if (total != 0)
+            {
+                int pos = (int)(((float)current) / total * 100);
+                this.progressBar1.Value = (int)pos;
+                this.progressPresent.Text = string.Format("{0}%", pos);
+                if (current == total)
+                {
+                    starttime = extractor.starttime;
+                    endtime = extractor.endtime;
+                    setInformation();
+                }
+            }
+        }
+        public void setInformation()
+        {
+            this.button4.Enabled = true;
+            this.button4.Text = "Extract";
+            if (!failToLink)
+            {
+                this.info.Text = "耗时：" + Convert.ToString((endtime - starttime) / 1000) + "秒";
+                this.info.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                this.info.Text = "Fail!";
+                this.info.ForeColor = System.Drawing.Color.Red;
+            }
+            this.progressBar1.Visible = false;
+            this.progressPresent.Visible = false;
+        }
+
+
+    }
+}
